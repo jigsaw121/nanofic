2225bf83625444d0700ae96a555939726c9dc9bc
jigsaw121 <tt.tuomas@gmail.com>
2013-12-28 22:09:36 +0200

    remove debug

--- nano.h
+++ nano.h
@@ -5,7 +5,6 @@
 typedef struct scene scene;
 scene* create(scene** data, char label[10], char* content, char* next);
 void add(scene** data, scene* s);
-char* defmsgbrd = "[e_bike2:Keep reading;e_bike2:Reply to this post]";
 void get_scenes(scene** data);
 
 #endif
